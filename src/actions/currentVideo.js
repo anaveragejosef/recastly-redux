f898115664b73bf0a21a7e5cb1acce1d778099f0
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-28 12:06:33 -0700

    Updates Action Items to Be Reflective of Action
    This updates the type components to accurately reflect what action is
    taking place.

--- src/actions/currentVideo.js
+++ src/actions/currentVideo.js
@@ -1,7 +1,7 @@
 var changeVideo = (video) => ({
   //TODO:  Return some action object to change the currently playing video.
   // Unique type to define action
-  type: "VIDEO_CLICK",
+  type: "VIDEO_PLAYER_CHANGE",
   // Pass new video object to update video player
   video: video
 });
--- src/actions/videoList.js
+++ src/actions/videoList.js
@@ -2,9 +2,9 @@ var changeVideoList = (videos) => ({
   //TODO:  Return some action object to change the list of videos in VideoList.
   // Typing new parameters into Search Field
   // Unique type to define action
-  type: "VIDEO_SEARCH",
+  type: "VIDEOS_UPDATE",
   // Pass new video object to update video player
-  searchQuery: searchQuery
+  videos: videos
 });
 
 export default changeVideoList;
