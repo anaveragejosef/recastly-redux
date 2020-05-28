f99fe5213fd8c9c04100bbd263604004a0380fd7
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-28 12:38:11 -0700

    Creates the Reducer Functions for Video Actions
    This creates the two functions that exist for the Current Video and
    Video List actions. This will update the state and allow the page
    to be rendered with new data.

--- src/reducers/currentVideo.js
+++ src/reducers/currentVideo.js
@@ -2,6 +2,12 @@ import Redux from 'redux';
 
 var currentVideoReducer = (state, action) => {
   //TODO: define a reducer for the currentVideo field of our state.
+  switch (action.type) {
+    case: 'VIDEO_PLAYER_CHANGE':
+      return state.currentVideo = action.video;
+    default:
+      return state;
+  }
 };
 
 export default currentVideoReducer;
