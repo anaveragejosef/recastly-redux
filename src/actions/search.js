438aa5b715baea5960764844ca6fe6fb451a931c
Josef Manfredonia <josefmanfredonia@gmail.com>
2020-05-28 12:36:50 -0700

    Updates Search Action to be Async
    After review, I have updated the search.js file to use an asynchronous
    action that calls a function to handle th search.

--- src/actions/search.js
+++ src/actions/search.js
@@ -6,8 +6,17 @@ import YOUTUBE_API_KEY from '../config/youtube.js';
 
 var handleVideoSearch = (q) => {
   //TODO:  Write an asynchronous action to handle a video search!
-  type: "VIDEO_SEARCH",
-  query: q
+  // Since this is async, we need to return a function and not an action
+  return (dispatch) => {
+    // Set the timeout so it is async
+    setTimeout(() => {
+      // Call dispatch on searchYouTube
+      dispatch(searchYouTube({YOUTUBE_API_KEY, q}, (data) => {
+        changeVideoList(data);
+        changeVideo(data[0]);
+      }));
+    }, 2000);
+  };
 };
 
 export default handleVideoSearch;
